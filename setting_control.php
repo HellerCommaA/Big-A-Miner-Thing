bf67c6584abfa3904877729a3e229f2bb66a68c9
Brandon <lamething@hotmail.com>
2011-07-25 19:06:18 -0400

    Removed an echo that wasn't displaying

--- setting_control.php
+++ setting_control.php
@@ -5,7 +5,7 @@ if(isset($_GET['mining'])){
 	
 		case 'stop':
 				$output = shell_exec("sudo /usr/sbin/stop_mining");
-				echo "<pre>$output</pre>";
+				//echo "<pre>$output</pre>";
 				break;
 		case 'start':
 				$output = shell_exec("sudo /usr/sbin/start_mining");
